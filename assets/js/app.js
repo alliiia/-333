321ef224bbea3ae6c08904f62474cf8dfea8ced5
alliiia <90026520+alliiia@users.noreply.github.com>
2022-06-21 20:42:01 +0700

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,6 @@ $(function() {
         scrollOffset = $(window).scrollTop();
 
 
-    /* Fixed Header */
     checkScroll(scrollOffset);
 
     $(window).on("scroll", function() {
@@ -23,25 +22,11 @@ $(function() {
     }
 
 
-    /* Smooth scroll */
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('scroll'),
-            blockOffset = $(blockId).offset().top;
-
-        $("#nav a").removeClass("active");
-        $this.addClass("active");
-
-        $("html, body").animate({
-            scrollTop:  blockOffset
-        }, 500);
-    });
 
 
 
-    /* Menu nav toggle */
+
+   
     $("#nav_toggle").on("click", function(event) {
         event.preventDefault();
 
@@ -51,23 +36,8 @@ $(function() {
 
 
 
-    /* Collapse */
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('collapse');
-
-        $this.toggleClass("active");
-    });
 
 
-    /* Slider */
-    $("[data-slider]").slick({
-        infinite: true,
-        fade: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
+
 
 });
